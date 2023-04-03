e59d985f62154a631abcf5686f52336de83f853a
Yousehyeon <dbtpgus111@naver.com>
2023-04-03 18:10:47 +0900

    array

--- ch02/js/seat.js
+++ ch02/js/seat.js
@@ -1,29 +1,23 @@
-        //입장객 수에 따른 좌석의 줄(행)수 계산
-        let customer = prompt("입장객은 몇 명인가요?");  //입장객 수
-        let colNum = prompt("한 줄에 몇명씩 앉나요? ");     //열 수
-        let rowNum;         //행(줄)의 수
+let customNum = parseInt(prompt("입장객은 몇 명인가요?")); //입장객 수
+let colNum = parseInt(prompt("한 줄에 몇 명 앉나요?"));   //열의 수
+let rowNum;    //행의 수
 
-        //나머지가 있는지 없는지 처리
-        if(customer == null || colNum == null){
-            document.write("<p>입력이 취소되엇습니다.</p>");
-        }
-        else{
-            if(parseInt(customer) % parseInt(colNum) == 0){
-            rowNum = customer / colNum;
-        }else{
-            rowNum = parseInt(customer / colNum) + 1;
-        }
-        // document.write(rowNum + "개의 줄이 필요합니다.")
-        }
-        document.write("<table>");
-        for(var i =0; i < rowNum; i++){
-            document.write("<tr>")
-            for(var j= 1; j <= colNum; j++){
-                var seatNum = colNum * i + j;  //좌석번호
-                if(seatNum > customer)  //좌석번호가 고객수보다 클때 빠져나옴
-                    break;
-                document.write("<td>좌석" + seatNum + "</td>");
-            }
-            document.write("<tr>");
-        }
-        document.write("</table>");
+if(customNum % colNum === 0){
+  rowNum = customNum / colNum;
+}
+else{ //나머지가 있으면 한 줄(행)이 더 필요
+  rowNum = parseInt(customNum / colNum) + 1;
+}
+
+document.write('<table>');
+for(let i = 0; i < rowNum; i++){
+  document.write('<tr>');
+  for(let j = 1; j <= colNum; j++){
+    let seatNum = colNum * i + j;
+    if(seatNum > customNum)
+      break;
+    document.write("<td>좌석" + seatNum + '</td>');
+  }
+  document.write('</tr>');
+}
+document.write('</table>');
